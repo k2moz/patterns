05a76230f819f699bc2c8a9eb1f4319319bea79f
a.sazhin <a.sazhin@itway.company>
2019-08-08 14:22:40 +0300

    init

---
+++ pat/pat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
